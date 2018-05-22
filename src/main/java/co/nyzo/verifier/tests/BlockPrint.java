91c4b1136ae2ba34220e4358508d35eb84c08f2d
Nyzo <nyzo@nyzo.co>
2018-05-22 17:20:31 -0400

    Added BlockPrint class.

---
+++ src/main/java/co/nyzo/verifier/tests/BlockPrint.java
@@ -0,0 +1,30 @@
+package co.nyzo.verifier.tests;
+
+import co.nyzo.verifier.Block;
+import co.nyzo.verifier.BlockManager;
+import co.nyzo.verifier.util.PrintUtil;
+
+public class BlockPrint {
+
+    public static void main(String[] args) {
+
+        long height = 0L;
+        try {
+            height = Long.parseLong(args[0]);
+        } catch (Exception ignored) { }
+
+        System.out.println("height:              " + height);
+        Block block = BlockManager.frozenBlockForHeight(height);
+        if (block == null) {
+            System.out.println("*** block is null ***");
+        } else {
+
+            System.out.println("height:                   " + height);
+            System.out.println("number of transactions:   " + block.getTransactions().size());
+            System.out.println("verifier:                 " +
+                    PrintUtil.compactPrintByteArray(block.getVerifierIdentifier()));
+            System.out.println("signature:                 " +
+                    PrintUtil.compactPrintByteArray(block.getVerifierSignature()));
+        }
+    }
+}
