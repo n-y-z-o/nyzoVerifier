c4e4d669d3905c0fac38d084ea7a0ee759767c7d
Nyzo <nyzo@nyzo.co>
2018-06-11 17:14:28 -0400

    Added size of voting verifiers to vote status.

--- src/main/java/co/nyzo/verifier/BlockVoteManager.java
+++ src/main/java/co/nyzo/verifier/BlockVoteManager.java
@@ -108,7 +108,7 @@ public class BlockVoteManager {
             if (height == BlockManager.frozenEdgeHeight() + 1L) {
                 StatusResponse.setField("vote", "m=" + maximumVotes + ", t=" + threshold + ", " +
                         PrintUtil.compactPrintByteArray(winningHash) + ", n=" + NodeManager.getMesh().size() +
-                        ", h=" + height);
+                        ", nv=" + votingVerifiers.size() + ", h=" + height);
             }
             StatusResponse.setField("in Genesis cycle", BlockManager.inGenesisCycle() + "");
         }
