0be6454e24f2cadf89ee7bbe8e4c0a58ed8b1b63
Nyzo <nyzo@nyzo.co>
2018-06-12 15:12:51 -0400

    Improved node-join test.

--- src/main/java/co/nyzo/verifier/tests/NodeJoinTest.java
+++ src/main/java/co/nyzo/verifier/tests/NodeJoinTest.java
@@ -12,12 +12,16 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 public class NodeJoinTest {
 
+    // This test sends a node-join message to check the response. It keeps sending until it receives a response that
+    // contains at least one vote.
+    private static final String node = "verifier0.nyzo.co";
+
     public static void main(String[] args) {
 
         AtomicInteger totalVotes = new AtomicInteger(0);
         while (totalVotes.get() == 0) {
             Message message = new Message(MessageType.NodeJoin3, new NodeJoinMessage());
-            Message.fetch("verifier0.nyzo.co", MeshListener.standardPort, message, false, new MessageCallback() {
+            Message.fetch(node, MeshListener.standardPort, message, false, new MessageCallback() {
                 @Override
                 public void responseReceived(Message message) {
 
