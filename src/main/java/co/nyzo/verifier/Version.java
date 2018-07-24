2fda1681f2c61ad74403649bd8e93597882248e3
Nyzo <nyzo@nyzo.co>
2018-07-23 23:34:31 -0400

    Added verifier name to block-fetch notification.

--- src/main/java/co/nyzo/verifier/Version.java
+++ src/main/java/co/nyzo/verifier/Version.java
@@ -2,7 +2,7 @@ package co.nyzo.verifier;
 
 public class Version {
 
-    private static final int version = 408;
+    private static final int version = 409;
 
     public static int getVersion() {
 
