06c67c534489a39b4d9fceaebeb8af19c4874527
Nyzo <nyzo@nyzo.co>
2019-01-10 14:24:31 -0500

    Version 485.

--- src/main/java/co/nyzo/verifier/Version.java
+++ src/main/java/co/nyzo/verifier/Version.java
@@ -2,7 +2,7 @@ package co.nyzo.verifier;
 
 public class Version {
 
-    private static final int version = 484;
+    private static final int version = 485;
 
     public static int getVersion() {
 
--- src/main/java/co/nyzo/verifier/messages/StatusResponse.java
+++ src/main/java/co/nyzo/verifier/messages/StatusResponse.java
@@ -11,93 +11,80 @@ import java.util.*;
 
 public class StatusResponse implements MessageObject {
 
-    private static long previousStatusTimestamp = 0L;
-    private static List<String> previousLines;
-
     private List<String> lines;
 
     public StatusResponse(byte[] requesterIdentifier) {
 
-        // To avoid unnecessary work, do not produce a status response more frequently than every four seconds.
-        long currentTimestamp = System.currentTimeMillis();
-        if (currentTimestamp - previousStatusTimestamp < 4000L) {
-            this.lines = previousLines;
-        } else {
+        long frozenEdgeHeight = BlockManager.getFrozenEdgeHeight();
+        Block frozenEdge = BlockManager.frozenBlockForHeight(frozenEdgeHeight);
 
-            long frozenEdgeHeight = BlockManager.getFrozenEdgeHeight();
-            Block frozenEdge = BlockManager.frozenBlockForHeight(frozenEdgeHeight);
-
-            List<String> lines = new ArrayList<>();
-            lines.add("nickname: " + Verifier.getNickname() + (Verifier.isPaused() ? "*** PAUSED ***" : ""));
-            if (TestnetUtil.testnet) {
-                lines.add("*** IN TESTNET MODE ***");
-            }
-            lines.add("version: " + Version.getVersion());
-            lines.add("ID: " + PrintUtil.compactPrintByteArray(Verifier.getIdentifier()));
-            lines.add("mesh: " + NodeManager.getNumberOfActiveIdentifiers() + " total, " +
-                    NodeManager.getNumberOfActiveCycleIdentifiers() + " in cycle");
-            lines.add("cycle length: " + BlockManager.currentCycleLength() +
-                    (BlockManager.inGenesisCycle() ? "(G)" : ""));
-            lines.add("transactions: " + TransactionPool.transactionPoolSize());
-            lines.add("retention edge: " + BlockManager.getRetentionEdgeHeight());
-            lines.add("trailing edge: " + BlockManager.getTrailingEdgeHeight());
-            lines.add("frozen edge: " + frozenEdgeHeight + " (" + (frozenEdge == null ? "null" :
-                    NicknameManager.get(frozenEdge.getVerifierIdentifier())) + ")");
-            lines.add("open edge: " + BlockManager.openEdgeHeight(false));
-            lines.add("blocks transmitted/created: " + Verifier.getBlockCreationInformation());
-            lines.add("votes requested: " + BlockVoteManager.getNumberOfVotesRequested());
-            List<Long> unfrozenBlockHeights = new ArrayList<>(UnfrozenBlockManager.unfrozenBlockHeights());
-            Collections.sort(unfrozenBlockHeights);
-            for (int i = 0; i < 7 && i < unfrozenBlockHeights.size(); i++) {
-                if (i == 3 && unfrozenBlockHeights.size() > 7) {
-                    lines.add("...");
-                } else {
-                    long height = i < 3 || unfrozenBlockHeights.size() <= 7 ? unfrozenBlockHeights.get(i) :
-                            unfrozenBlockHeights.get(unfrozenBlockHeights.size() - 7 + i);
-                    if (height > frozenEdgeHeight) {
-                        String heightString = "+" + (height - frozenEdgeHeight);
-                        lines.add("- h: " + heightString + ", n: " +
-                                UnfrozenBlockManager.numberOfBlocksAtHeight(height) +
-                                ", v: " + BlockVoteManager.votesAtHeight(height));
-                    }
-                }
-            }
-            lines.add("requester identifier: " + PrintUtil.compactPrintByteArray(requesterIdentifier));
-
-            if (ByteUtil.arraysAreEqual(requesterIdentifier, Verifier.getIdentifier())) {
-                lines.add("new timestamp: " + Verifier.newestTimestampAge(2));
-                lines.add("old timestamp: " + Verifier.oldestTimestampAge());
-                lines.add("blocks: " + BlockManagerMap.mapInformation());
-                lines.add("balance lists: " + BalanceListManager.mapInformation());
-                lines.add("node-joins sent: " + NodeManager.getNodeJoinRequestsSent());
-                lines.add("memory (min/max/avg): " + MemoryMonitor.getMemoryStats());
-
-                Map<Long, Integer> thresholdOverrides = UnfrozenBlockManager.getThresholdOverrides();
-                for (Long height : thresholdOverrides.keySet()) {
-                    lines.add("override @+" + (height - frozenEdgeHeight) + ": " + thresholdOverrides.get(height) + "%");
-                }
-
-                Map<Long, byte[]> hashOverrides = UnfrozenBlockManager.getHashOverrides();
-                for (Long height : hashOverrides.keySet()) {
-                    lines.add("override @+" + (height - frozenEdgeHeight) + ": " +
-                            PrintUtil.superCompactPrintByteArray(hashOverrides.get(height)));
-                }
-
-                // The notification budget is initialized to -1. A non-negative value is only set if the notification
-                // endpoint is configured, so this will not be displayed if notifications are not used.
-                long notificationBudget = NotificationUtil.getCurrentBudget();
-                if (notificationBudget >= 0) {
-                    lines.add("notif. budget: " + notificationBudget);
-                }
-
-                // This shows which in-cycle verifiers currently have no active mesh nodes.
-                lines.add("missing in-cycle verifiers: " + NodeManager.getMissingInCycleVerifiers());
-            }
-
-            this.lines = lines;
-            previousLines = lines;
-            previousStatusTimestamp = currentTimestamp;
+        List<String> lines = new ArrayList<>();
+        lines.add("nickname: " + Verifier.getNickname() + (Verifier.isPaused() ? "*** PAUSED ***" : ""));
+        if (TestnetUtil.testnet) {
+            lines.add("*** IN TESTNET MODE ***");
         }
+        lines.add("version: " + Version.getVersion());
+        lines.add("ID: " + PrintUtil.compactPrintByteArray(Verifier.getIdentifier()));
+        lines.add("mesh: " + NodeManager.getNumberOfActiveIdentifiers() + " total, " +
+                NodeManager.getNumberOfActiveCycleIdentifiers() + " in cycle");
+        lines.add("cycle length: " + BlockManager.currentCycleLength() + (BlockManager.inGenesisCycle() ? "(G)" : ""));
+        lines.add("transactions: " + TransactionPool.transactionPoolSize());
+        lines.add("retention edge: " + BlockManager.getRetentionEdgeHeight());
+        lines.add("trailing edge: " + BlockManager.getTrailingEdgeHeight());
+        lines.add("frozen edge: " + frozenEdgeHeight + " (" + (frozenEdge == null ? "null" :
+                NicknameManager.get(frozenEdge.getVerifierIdentifier())) + ")");
+        lines.add("open edge: " + BlockManager.openEdgeHeight(false));
+        lines.add("blocks transmitted/created: " + Verifier.getBlockCreationInformation());
+        lines.add("votes requested: " + BlockVoteManager.getNumberOfVotesRequested());
+        List<Long> unfrozenBlockHeights = new ArrayList<>(UnfrozenBlockManager.unfrozenBlockHeights());
+        Collections.sort(unfrozenBlockHeights);
+        for (int i = 0; i < 7 && i < unfrozenBlockHeights.size(); i++) {
+            if (i == 3 && unfrozenBlockHeights.size() > 7) {
+                lines.add("...");
+            } else {
+                long height = i < 3 || unfrozenBlockHeights.size() <= 7 ? unfrozenBlockHeights.get(i) :
+                        unfrozenBlockHeights.get(unfrozenBlockHeights.size() - 7 + i);
+                if (height > frozenEdgeHeight) {
+                    String heightString = "+" + (height - frozenEdgeHeight);
+                    lines.add("- h: " + heightString + ", n: " +
+                            UnfrozenBlockManager.numberOfBlocksAtHeight(height) +
+                            ", v: " + BlockVoteManager.votesAtHeight(height));
+                }
+            }
+        }
+        lines.add("requester identifier: " + PrintUtil.compactPrintByteArray(requesterIdentifier));
+
+        if (ByteUtil.arraysAreEqual(requesterIdentifier, Verifier.getIdentifier())) {
+            lines.add("new timestamp: " + Verifier.newestTimestampAge(2));
+            lines.add("old timestamp: " + Verifier.oldestTimestampAge());
+            lines.add("blocks: " + BlockManagerMap.mapInformation());
+            lines.add("balance lists: " + BalanceListManager.mapInformation());
+            lines.add("node-joins sent: " + NodeManager.getNodeJoinRequestsSent());
+            lines.add("memory (min/max/avg): " + MemoryMonitor.getMemoryStats());
+
+            Map<Long, Integer> thresholdOverrides = UnfrozenBlockManager.getThresholdOverrides();
+            for (Long height : thresholdOverrides.keySet()) {
+                lines.add("override @+" + (height - frozenEdgeHeight) + ": " + thresholdOverrides.get(height) + "%");
+            }
+
+            Map<Long, byte[]> hashOverrides = UnfrozenBlockManager.getHashOverrides();
+            for (Long height : hashOverrides.keySet()) {
+                lines.add("override @+" + (height - frozenEdgeHeight) + ": " +
+                        PrintUtil.superCompactPrintByteArray(hashOverrides.get(height)));
+            }
+
+            // The notification budget is initialized to -1. A non-negative value is only set if the notification
+            // endpoint is configured, so this will not be displayed if notifications are not used.
+            long notificationBudget = NotificationUtil.getCurrentBudget();
+            if (notificationBudget >= 0) {
+                lines.add("notif. budget: " + notificationBudget);
+            }
+
+            // This shows which in-cycle verifiers currently have no active mesh nodes.
+            lines.add("missing in-cycle verifiers: " + NodeManager.getMissingInCycleVerifiers());
+        }
+
+        this.lines = lines;
     }
 
     private StatusResponse(List<String> lines) {
