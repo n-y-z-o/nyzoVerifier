740a5c8a9814acb4d1c6763f0257bf91de1663e5
Nyzo <nyzo@nyzo.co>
2019-03-03 21:48:30 -0500

    Version 500. Corrected stability issue with nickname manager.

--- src/main/java/co/nyzo/verifier/NicknameManager.java
+++ src/main/java/co/nyzo/verifier/NicknameManager.java
@@ -3,20 +3,28 @@ package co.nyzo.verifier;
 import co.nyzo.verifier.util.PrintUtil;
 
 import java.nio.ByteBuffer;
-import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class NicknameManager {
 
-    private static final Map<ByteBuffer, String> nicknameMap = new HashMap<>();
+    private static final int maximumNicknameLength = 32;
+
+    private static final Map<ByteBuffer, String> nicknameMap = new ConcurrentHashMap<>();
 
     public static void put(byte[] identifier, String nickname) {
 
-        if (nickname != null) {
-            nickname = nickname.trim();
-        }
+        // If the nickname map has more than 2000 entries, accept nicknames from in-cycle verifiers only. Even if the
+        // first 2000 nicknames are all from out-of-cycle verifiers, the maximum map size will be 2000 + [cycle size],
+        // and cycle size is guaranteed by the blockchain rules to grow slowly. This will protect against memory issues
+        // both from deliberate attacks and large numbers of out-of-cycle verifiers. Also, do not process null nicknames
+        // or absurdly long nicknames.
+        if (nickname != null && nickname.length() < maximumNicknameLength &&
+                (nicknameMap.size() < 2000 || BlockManager.verifierInCurrentCycle(ByteBuffer.wrap(identifier)))) {
 
-        nicknameMap.put(ByteBuffer.wrap(identifier), nickname);
+            nickname = nickname.trim();
+            nicknameMap.put(ByteBuffer.wrap(identifier), nickname);
+        }
     }
 
     public static String get(byte[] identifier) {
