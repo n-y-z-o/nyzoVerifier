0b95d52196cba41b041cb20f63003a91f89345ec
Nyzo <nyzo@nyzo.co>
2019-07-20 18:11:29 -0400

    Version 533. Updated HashVoteOverrideRequestScript for easier local operation.

--- src/main/java/co/nyzo/verifier/Version.java
+++ src/main/java/co/nyzo/verifier/Version.java
@@ -2,7 +2,7 @@ package co.nyzo.verifier;
 
 public class Version {
 
-    private static final int version = 532;
+    private static final int version = 533;
 
     public static int getVersion() {
 
--- src/main/java/co/nyzo/verifier/scripts/HashVoteOverrideRequestScript.java
+++ src/main/java/co/nyzo/verifier/scripts/HashVoteOverrideRequestScript.java
@@ -5,6 +5,7 @@ import co.nyzo.verifier.messages.HashVoteOverrideRequest;
 import co.nyzo.verifier.util.IpUtil;
 import co.nyzo.verifier.util.UpdateUtil;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -12,9 +13,14 @@ public class HashVoteOverrideRequestScript {
 
     public static void main(String[] args) {
 
-        if (args.length != 3) {
+        if (args.length != 2 && args.length != 3) {
             System.out.println("\n\n\n*****************************************************************");
             System.out.println("required arguments:");
+            System.out.println("*** local operation ***");
+            System.out.println("- height of the override block vote");
+            System.out.println("- hash of the override block vote");
+            System.out.println("OR");
+            System.out.println("*** remote operation ***");
             System.out.println("- private seed of your in-cycle verifier");
             System.out.println("- height of the override block vote");
             System.out.println("- hash of the override block vote");
@@ -22,30 +28,41 @@ public class HashVoteOverrideRequestScript {
             return;
         }
 
-        // Get the private seed and corresponding identifier that was provided as the argument.
-        byte[] privateSeed = ByteUtil.byteArrayFromHexString(args[0], FieldByteSize.seed);
-        byte[] inCycleVerifierIdentifier = KeyUtil.identifierForSeed(privateSeed);
+        boolean localOperation = args.length == 2;
 
-        // Get the IP addresses of the verifier.
-        List<byte[]> ipAddresses = ScriptUtil.ipAddressesForVerifier(inCycleVerifierIdentifier);
-        if (ipAddresses.isEmpty()) {
-            System.out.println("unable to find IP address of " +
-                    ByteUtil.arrayAsStringWithDashes(inCycleVerifierIdentifier));
+        // Get the private seed and corresponding identifier that was provided as the argument.
+        byte[] privateSeed;
+        List<byte[]> ipAddresses;
+        if (localOperation) {
+            privateSeed = null;
+            ipAddresses = Arrays.asList(IpUtil.addressFromString("127.0.0.1"));
+        } else {
+            privateSeed = ByteUtil.byteArrayFromHexString(args[0], FieldByteSize.seed);
+            byte[] inCycleVerifierIdentifier = KeyUtil.identifierForSeed(privateSeed);
+
+            // Get the IP addresses of the verifier.
+            ipAddresses = ScriptUtil.ipAddressesForVerifier(inCycleVerifierIdentifier);
+            if (ipAddresses.isEmpty()) {
+                System.out.println("unable to find IP address of " +
+                        ByteUtil.arrayAsStringWithDashes(inCycleVerifierIdentifier));
+            }
         }
 
         // Get the height and hash from the arguments.
         long height = 0L;
         try {
-            height = Long.parseLong(args[1]);
+            height = Long.parseLong(localOperation ? args[0] : args[1]);
         } catch (Exception ignored) { }
 
-        byte[] hash = ByteUtil.byteArrayFromHexString(args[2], FieldByteSize.hash);
+        byte[] hash = ByteUtil.byteArrayFromHexString(localOperation ? args[1] : args[2], FieldByteSize.hash);
 
         // Send the request to our verifier instances.
         AtomicInteger numberOfResponsesNotYetReceived = new AtomicInteger(ipAddresses.size());
         HashVoteOverrideRequest request = new HashVoteOverrideRequest(height, hash);
         Message message = new Message(MessageType.HashVoteOverrideRequest29, request);
-        message.sign(privateSeed);
+        if (privateSeed != null) {
+            message.sign(privateSeed);
+        }
         for (byte[] ipAddress : ipAddresses) {
             Message.fetchTcp(IpUtil.addressAsString(ipAddress), MeshListener.standardPortTcp, message,
                     new MessageCallback() {
