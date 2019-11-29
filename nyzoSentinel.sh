2b4602a7c7cf25fe148e544afad2d0db0c5ad8c4
Nyzo <nyzo@nyzo.co>
2019-11-29 11:22:19 -0500

    Version 555. Updated configuration scripts.

--- nyzoSentinel.sh
+++ nyzoSentinel.sh
@@ -1,16 +1,16 @@
-CONFIGURATION_FILE_PATH=./nyzoDocumentationServer.conf
+CONFIGURATION_FILE_PATH=./nyzoSentinel.conf
 VERIFIER_ROOT=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 sudo rm $CONFIGURATION_FILE_PATH 1>/dev/null 2>/dev/null
 
 cat << END_OF_FILE >> $CONFIGURATION_FILE_PATH
-[program:nyzo_documentation_server]
-command=/usr/bin/java -jar -Xmx3G $VERIFIER_ROOT/build/libs/nyzoVerifier-1.0.jar co.nyzo.verifier.documentation.DocumentationServer
+[program:nyzo_sentinel]
+command=/usr/bin/java -jar -Xmx3G $VERIFIER_ROOT/build/libs/nyzoVerifier-1.0.jar co.nyzo.verifier.sentinel.Sentinel
 autostart=true
 autorestart=true
 startsecs=10
 startretries=20
-stdout_logfile=/var/log/nyzo-documentation-server.log
+stdout_logfile=/var/log/nyzo-sentinel.log
 stdout_logfile_maxbytes=10MB
 stdout_logfile_backups=2
 redirect_stderr=true
