fe114481571a005d5ab9c8a0aecdbf3a336f150c
Nyzo <nyzo@nyzo.co>
2022-12-06 19:28:04 -0500

    Version 636. Social meta tags.

--- src/main/java/co/nyzo/verifier/Version.java
+++ src/main/java/co/nyzo/verifier/Version.java
@@ -2,7 +2,7 @@ package co.nyzo.verifier;
 
 public class Version {
 
-    private static final int version = 635;
+    private static final int version = 636;
 
     public static int getVersion() {
 
--- src/main/java/co/nyzo/verifier/web/elements/Head.java
+++ src/main/java/co/nyzo/verifier/web/elements/Head.java
@@ -1,5 +1,6 @@
 package co.nyzo.verifier.web.elements;
 
+import co.nyzo.verifier.util.PreferencesUtil;
 public class Head extends HtmlTag {
 
     @Override
@@ -15,11 +16,42 @@ public class Head extends HtmlTag {
     }
 
     public Head addStandardMetadata(String title) {
+        return addStandardMetadata(title, title);
+    }
+
+    public Head addStandardMetadata(String title, String description) {
         addStandardMetadata();
         add(new Title(title));
+
+        // Get the social image URL, width, and height from preferences. This process could be made more dynamic, if
+        // necessary.
+        String imageUrl = PreferencesUtil.get("social_image_url", null);
+        int imageWidth = PreferencesUtil.getInt("social_image_width", -1);
+        int imageHeight = PreferencesUtil.getInt("social_image_height", -1);
+
+        // Add Facebook Open Graph metadata.
         add(new Meta().attr("property", "og:title").attr("content", title));
-        add(new Meta().attr("property", "og:description").attr("content", title));
+        add(new Meta().attr("property", "og:description").attr("content", description));
         add(new Meta().attr("property", "og:type").attr("content", "website"));
+        if (imageUrl != null && imageWidth > 0 && imageHeight > 0) {
+            add(new Meta().attr("property", "og:image").attr("content", imageUrl));
+            add(new Meta().attr("property", "og:image:width").attr("content", imageWidth + ""));
+            add(new Meta().attr("property", "og:image:height").attr("content", imageHeight + ""));
+        }
+
+        // Add Twitter Card metadata.
+        add(new Meta().attr("name", "twitter:card").attr("content", "summary_large_image"));
+        add(new Meta().attr("name", "twitter:site").attr("content", PreferencesUtil.get("twitter_site", "@Nyzo16")));
+        add(new Meta().attr("name", "twitter:creator").attr("content", PreferencesUtil.get("twitter_creator",
+                "@Nyzo16")));
+        add(new Meta().attr("name", "twitter:title").attr("content", title));
+        if (!description.equals(title)) {
+            add(new Meta().attr("name", "twitter:description").attr("content", description));
+        }
+        if (imageUrl != null) {
+            add(new Meta().attr("name", "twitter:image").attr("content", imageUrl));
+        }
+
         return this;
     }
 }
